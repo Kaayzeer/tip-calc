663b16ef1e838e79657b041098f8282d07f362a5
nikolas.rosinelli@weknowit.se <mautiagiro@gmail.com>
2022-02-04 08:39:38 +0100

    filetree setup, neseccary installations, most of the css inte desktop mode done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+//styles
+import "./sass/App.scss";
+
+//components
+import Title from "./Title";
 
 function App() {
+  const tipAmount = ["5%", "10%", "15%", "25%", "50%"];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-container">
+        <Title />
+        <div className="calc-container">
+          <div className="section-1">
+            <form>
+              <label className="label __text">
+                {" "}
+                Bill
+                <input type="text" className="input bill" />
+              </label>
+            </form>
+            <div className="select-tip">
+              <h3 className="__text">select tip</h3>
+              {tipAmount.map((amount, index) => (
+                <button key={index} className="tip-button">
+                  {amount}
+                </button>
+              ))}
+              <input
+                type="text"
+                placeholder="Custom"
+                className="custom-button"
+              />
+            </div>
+            <form>
+              <label className="label __text">
+                {" "}
+                Number of People
+                <input type="select" className="input people" />
+              </label>
+            </form>
+          </div>
+          <div className="section-2">
+            <div className="tip-amount">
+              <p className="tip-amount-text">tip amount</p>
+              <span className="person">/ person</span>
+              <h1 className="amount">$0.00</h1>
+            </div>
+            <div className="total-amount">
+              <p className="total-amount-text">total amount</p>
+              <span className="person">/ person</span>
+              <h1 className="amount">$0.00</h1>
+            </div>
+            <button className="reset-button">reset</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Title.js
@@ -0,0 +1,10 @@
+const Title = () => {
+  return (
+    <div className="u-title-main">
+      <span className="main-title-first-part">spli</span>
+      <span className="main-title-second-part">tter</span>
+    </div>
+  );
+};
+
+export default Title;
